e0dc0c65ba128a46641b93824409893ca19ef2f3
kashish-2003 <kashishrithe003@gmail.com>
2026-03-13 13:50:26 +0530

    updated server file

--- server.js
+++ server.js
@@ -2,34 +2,39 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-
+ 
+ 
 const stockRouter = require("./routes/stockRouter");
 const authRoutes = require("./routes/authRoutes");
 const depositRoutes = require("./routes/depositRoutes");
 const withdrawRoutes = require("./routes/withdrawRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const walletRoutes = require("./routes/walletRoutes");
-
+const watchlistRoutes = require("./routes/watchlistRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 const app = express();
-
+ 
 // Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+ 
 // Static folder for screenshots
 app.use("/uploads", express.static("uploads"));
-
+ 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/deposit", depositRoutes);
 app.use("/api/withdraw", withdrawRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/wallet", walletRoutes);
+app.use("/api/watchlist", watchlistRoutes);
+app.use("/api/admin", adminRoutes);
+ 
 app.use("/api", stockRouter);
-
+ 
 // Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
+ 
