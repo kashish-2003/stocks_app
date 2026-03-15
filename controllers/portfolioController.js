e3c70f51307e06e53f9ad0e25890292e7abc4c69
Paras vanve <parasvanve218@gmail.com>
2026-03-15 13:19:56 +0530

    portfolio api changes

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,23 +1,46 @@
 const portfolioService = require("../services/portfolioService");
 
 exports.getPortfolio = async (req, res) => {
-
   try {
-
-    const user_id = req.user.id;
+    const user_id = req.user.id; 
 
     const portfolio = await portfolioService.getPortfolio(user_id);
 
     res.json({
+      success: true,
       portfolio
     });
 
   } catch (error) {
-
     res.status(500).json({
-      error: error.message
+      success: false,
+      message: error.message
     });
-
   }
+};
+
+
+exports.exitPosition = (req, res) => {
+
+  const user_id = req.user.id;   
+  const { portfolio_id } = req.body;
+
+  portfolioService.exitPosition(user_id, portfolio_id)
+    .then(result => {
+
+      res.json({
+        success: true,
+        message: "Position exited successfully"
+      });
+
+    })
+    .catch(err => {
+
+      res.json({
+        success: false,
+        message: err.message
+      });
+
+    });
 
 };
